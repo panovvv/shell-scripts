65ce78a76045de9aa1ae312726773a910197bc9c
Vadim Panov <vpanov@griddynamics.com>
2019-12-24 21:17:34 +0100

    upgrade rename_vids
    now it seeks multiple regular expressions.

--- media/rename_vids.sh
+++ media/rename_vids.sh
@@ -4,40 +4,51 @@
 TIMESTAMP_SOURCE=n
 
 # Regex that matches timestamp in filename
-TIMESTAMP_REGEX="[0-9]{14}"
-
-renameVideo () {
+function sharearrays() {
+	TIMESTAMP_REGEX=("[0-9]{14}" "[0-9]{8}_[0-9]{6}")
 	# Above regex will single out timestamp from name.
 	# Variables below define where in this string we have 
 	# every portion of date and time.
-	YEAR_POSITION=0
-	MONTH_POSITION=4
-	DAY_POSITION=6
-	HOUR_POSITION=8
-	MINUTE_POSITION=10
-	SECOND_POSITION=12
+	YEAR_POSITION=(0 0)
+	MONTH_POSITION=(4 4)
+	DAY_POSITION=(6 6)
+	HOUR_POSITION=(8 9)
+	MINUTE_POSITION=(10 11)
+	SECOND_POSITION=(12 13)
+}
 
+renameVideo () {
 	echo
 	filename=$(basename -- "$1")
 	ext="${filename##*.}"
 	ext=`echo "$ext" | tr '[:upper:]' '[:lower:]'`;
 	case "${TIMESTAMP_SOURCE}" in
-		n*)	ts=`echo $(basename "$1") | grep -oE $TIMESTAMP_REGEX`
-			#any datetime stamp will contain at least 14 difits: 8 for date and 6 for time
-			if [[ ${#ts} -lt 14 ]] 
+		n*)
+			found=false
+			for i in "${!TIMESTAMP_REGEX[@]}";
+			do
+				ts=`echo $(basename "$1") | grep -oE "${TIMESTAMP_REGEX[$i]}"`
+
+				#any datetime stamp will contain at least 14 digits: 8 for date and 6 for time
+			   	if [[ ${#ts} -gt 13 ]] 
+				then
+					y=${ts:${YEAR_POSITION[$i]}:4}
+					mo=${ts:${MONTH_POSITION[$i]}:2}
+					d=${ts:${DAY_POSITION[$i]}:2}
+					h=${ts:${HOUR_POSITION[$i]}:2}
+					mi=${ts:${MINUTE_POSITION[$i]}:2}
+					s=${ts:${SECOND_POSITION[$i]}:2}
+					finalName="${y}-${mo}-${d}_${h}-${mi}-${s}.${ext}"
+					echo "Processing \"$(basename "$1")\"... Found timestamp \"${ts}\"! Renaming to ${finalName}"
+					DIR=$(dirname "$1")
+					mv -vn "$1" "${DIR}/${finalName}"
+					found=true
+					break
+				fi
+			done
+			if [ "$found" = false ]
 			then
 				echo "Processing \"$(basename "$1")\"... Can't find timestamp in name! Skipping..."
-			else
-				y=${ts:$YEAR_POSITION:4}
-				mo=${ts:$MONTH_POSITION:2}
-				d=${ts:$DAY_POSITION:2}
-				h=${ts:$HOUR_POSITION:2}
-				mi=${ts:$MINUTE_POSITION:2}
-				s=${ts:$SECOND_POSITION:2}
-				finalName="${y}-${mo}-${d}_${h}-${mi}-${s}.${ext}"
-				echo "Processing \"$(basename "$1")\"... Found timestamp \"${ts}\"! Renaming to ${finalName}"
-				DIR=$(dirname "$1")
-				mv -vn "$1" "${DIR}/${finalName}"
 			fi
 			;;
 		c*)	finalName="$(stat -f %SB -t %Y-%m-%d_%H-%M-%S "$1").${ext}"
@@ -66,6 +77,8 @@ renameVideo () {
 
 } 
 
+sharearray
+
 if [[ -z "$1" ]] || [[ "$1" = "-h" ]] || [[ "$1" = "--help" ]] ;
 then
 	echo "Usage: $(basename "$0") <directory> TIMESTAMP_SOURCE"
@@ -75,7 +88,7 @@ then
 	echo "	   Extract the timestamp and rename accordingly."
 	echo "	   Likely use case: video from mobile phones."
 	echo "	   Example filename: V_20181118_192009_N0.mp4 - will be renamed to 2018-11-18_19-20-09.mp4"
-	echo "	   Make sure that TIMESTAMP_REGEX variable contains a regex matching the timestamp format."
+	echo "	   Make sure that TIMESTAMP_REGEX variable contains at least one regex matching the timestamp format."
 	echo "	c: file doesn't contain timestamp in filename."
 	echo "	   Will rename files to their CREATION date."
 	echo "	   Likely use case: video from compact cameras."
@@ -111,7 +124,12 @@ then
 fi
 
 case "${TIMESTAMP_SOURCE}" in
-	n*)		echo "Sourcing timestamp from file name, regular expression: ${TIMESTAMP_REGEX}";;
+	n*)
+			echo "Sourcing timestamp from file name, regular expressions:"
+			for i in "${TIMESTAMP_REGEX[@]}"
+			do
+				echo "$i"
+			done ;;
 	c*)		echo "Sourcing timestamp from file's creation date";;
 	m*)		echo "Sourcing timestamp from file's modification date";;
 	*)		echo "Bad TIMESTAMP_SOURCE specified!"; exit 1
@@ -127,15 +145,15 @@ read -p "$message" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+	export -f sharearrays
 	export -f renameVideo
 	export TIMESTAMP_SOURCE
-	export TIMESTAMP_REGEX
 	export fullDirName
 
 	# Show a line of dashes
 	printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 	
-	find "${fullDirName}" -type f \( -iname "*.mov" -or -iname "*.mp4" -or -iname "*.3gp" -or -iname "*.mts" -or -iname "*.avi" -or -iname "*.dat" \) -exec bash -c 'renameVideo "$0"' {} \;
+	find "${fullDirName}" -type f \( -iname "*.mov" -or -iname "*.mp4" -or -iname "*.3gp" -or -iname "*.mts" -or -iname "*.avi" -or -iname "*.dat" \) -exec bash -c 'sharearrays; renameVideo "$0"' {} \;
 	
 	# Show a line of dashes
 	printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
